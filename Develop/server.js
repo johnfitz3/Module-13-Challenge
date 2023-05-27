602a2281e6c6f61f7cabc4e7485186809a79b9f2
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-05-27 16:56:29 -0400

    starting coding

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
+const { Sequelize } = require('sequelize');
+const mysql12 = require('mysql12');
+
 // import sequelize connection
+const sequelize = new Sequelize('sqlite::memory:')
+//testing sequelize connection
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 const app = express();
 const PORT = process.env.PORT || 3001;
